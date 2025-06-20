cd90bab6b32d0ffff61164340aea37550a057817
Zakaria-Tajer <za.tajer@gmail.com>
2025-06-20 18:21:18 +0100

    Starting with Task-Flow Frontend

---
+++ Frontend/TaskFlow-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Auth from "./components/Auth";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Auth />
+      </div>
+    </>
+  );
+}
+
+export default App;
