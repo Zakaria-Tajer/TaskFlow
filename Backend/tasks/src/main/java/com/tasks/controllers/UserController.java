f0ba6a6a2478066858922fb6bb57a7a2e3dfe203
Zakaria-Tajer <za.tajer@gmail.com>
2025-06-20 12:48:55 +0100

    adding a user controller

---
+++ Backend/tasks/src/main/java/com/tasks/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.tasks.controllers;
+
+
+import com.tasks.dto.UserDto;
+import com.tasks.dto.UserLoginDto;
+import com.tasks.dto.UserResponseDto;
+import com.tasks.services.User.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api/v1/auth/")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+
+
+    public final UserService userService;
+
+    @PostMapping("register")
+    public ResponseEntity<UserResponseDto> SignUp(@Valid @RequestBody UserDto userDto) {
+        String token = userService.SignUp(userDto);
+
+        return ResponseEntity.ok(UserResponseDto.builder().message("User Signed Up successfully")
+                .statusCode(HttpStatus.OK)
+                .timestamp(Date.from(Instant.now()))
+                .token(token)
+                .build());
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<UserResponseDto> SignIn(@Valid @RequestBody UserLoginDto userLoginDto) {
+
+        String token = userService.SignIn(userLoginDto);
+
+        return ResponseEntity.ok(UserResponseDto.builder().message("User Signed In successfully")
+                .statusCode(HttpStatus.OK)
+                .timestamp(Date.from(Instant.now()))
+                .token(token)
+                .build());
+    }
+
+}
