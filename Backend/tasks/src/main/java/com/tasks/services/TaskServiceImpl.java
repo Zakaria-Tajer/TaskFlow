c4eace82fa42bc5b11b7a37eb822656218892b38
Zakaria-Tajer <za.tajer@gmail.com>
2025-06-20 10:45:40 +0100

    implementing exceptions and refactoring task service

--- Backend/tasks/src/main/java/com/tasks/services/TaskServiceImpl.java
+++ Backend/tasks/src/main/java/com/tasks/services/TaskServiceImpl.java
@@ -2,9 +2,12 @@ package com.tasks.services;
 
 import com.tasks.dto.TaskRequestDto;
 import com.tasks.dto.TaskResponseDto;
+import com.tasks.exceptions.FunctionalException;
 import com.tasks.mappers.TaskMapper;
 import com.tasks.models.Task;
+import com.tasks.models.User;
 import com.tasks.repository.TasksRepository;
+import com.tasks.repository.UserRepository;
 import com.tasks.services.Task.TaskService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,63 +27,59 @@ public class TaskServiceImpl implements TaskService {
 
 
     public final TasksRepository tasksRepository;
+    public final UserRepository userRepository;
+
+
 
     @Override
     public Task createTask(TaskRequestDto task) {
 
         log.info("Creating task {}", task.getStatus());
 
-        Task newTask = TaskMapper.toEntity(task);
+        User user = userRepository.findById(task.getUserId())
+                .orElseThrow(() -> new FunctionalException("USER_NOT_FOUND", HttpStatus.NOT_FOUND));
 
+        Task newTask = TaskMapper.toEntity(task);
+        newTask.setUser(user);
 
         return tasksRepository.save(newTask);
     }
 
     @Override
     public Task updateTask(Long TaskID, TaskRequestDto task) {
-        Optional<Task> existingTask = tasksRepository.findById(TaskID);
+        Task existingTask = getTaskById(TaskID);
 
-//        Need to implement the logic for empty task
-        if (existingTask.isEmpty()) {}
-
-        Task existingTaskEntity = existingTask.get();
-
-        existingTaskEntity.setTitle(task.getTitle() != null ? task.getTitle() : existingTaskEntity.getTitle());
-        existingTaskEntity.setStatus(task.getStatus() != null ? task.getStatus() : existingTaskEntity.getStatus());
-        existingTaskEntity.setDescription(task.getDescription() != null ? task.getDescription() : existingTaskEntity.getDescription());
+        existingTask.setTitle(task.getTitle() != null ? task.getTitle() : existingTask.getTitle());
+        existingTask.setStatus(task.getStatus() != null ? task.getStatus() : existingTask.getStatus());
+        existingTask.setDescription(task.getDescription() != null ? task.getDescription() : existingTask.getDescription());
 
         log.info("Updating task {}", task.getStatus());
-        return tasksRepository.save(existingTaskEntity);
+        return tasksRepository.save(existingTask);
     }
 
     @Override
     public void deleteTask(Long TaskID) {
-        Optional<Task> existingTask = tasksRepository.findById(TaskID);
-
-        //   Also with this / Need to implement the logic for empty task
-
-        if (existingTask.isEmpty()) {}
-
-        Task taskExists = existingTask.get();
-
+        Task taskExists = getTaskById(TaskID);
 
         log.info("Deleting task {}", taskExists.getTitle());
         tasksRepository.delete(taskExists);
     }
 
-    @Override
-    public Task getTaskById(Long id) {
-        return null;
-    }
-
 
     @Override
     public List<TaskResponseDto> getAllTasks() {
-        return tasksRepository.findAll()
+        return tasksRepository.findAll(Sort.by(Sort.Direction.ASC, "id"))
                 .stream()
                 .map(TaskMapper::toResponseDTO)
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public Task getTaskById(Long TaskID) {
+        return tasksRepository.findById(TaskID)
+                .orElseThrow(() -> new FunctionalException("TASK_NOT_FOUND", HttpStatus.NOT_FOUND));
+    }
+
 
 
 
